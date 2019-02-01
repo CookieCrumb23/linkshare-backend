bbbc6691690b91c1acba5aaf490b88c6879a39ba
CookieCrumb23 <CookieCrumb23@users.noreply.github.com>
2019-02-01 13:31:25 +0100

    Refactor and restructure code to be more fault tolerant.

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,96 @@
 #![feature(proc_macro_hygiene, decl_macro)]
-
 extern crate env_logger;
 extern crate log;
 #[macro_use]
 extern crate rocket;
 
 use std::fs::{File, OpenOptions};
-use std::io::{Result, Write};
+use std::io::{BufRead, BufReader, Write};
 
-use rocket::http::Status;
-use rocket_contrib::json::Json;
-use serde_derive::Deserialize;
+use serde_derive::{Deserialize, Serialize};
 
-#[derive(Deserialize, Debug)]
-struct Link {
-	url: String,
-	read: bool,
+type Result<T> = std::result::Result<T, Box<std::error::Error>>;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Link {
+    url: String,
+    read: bool,
 }
 
-#[post("/link", data = "<link>")]
-fn post(link: Json<Link>) -> Status {
-	println!("{:?}", link);
-	let file = get_tmp_file();
+mod routes {
+    use rocket::http::Status;
+    use std::result::Result;
 
-	let mut foo = match file {
-		Ok(t) => t,
-		Err(e) => panic!(e)
-	};
+    use rocket_contrib::json::Json;
+    type Link = super::Link;
 
-	let result = write!(foo, "{:?},{:?}", link.url, link.read);
+    #[post("/link", data = "<link>")]
+    pub fn post(link: Json<Link>) -> Status {
+        match super::save_link(&link) {
+            Ok(_) => Status::NoContent,
+            Err(_) => Status::InternalServerError,
+        }
+    }
 
-	match result {
-		Ok(_) => Status::NoContent,
-		Err(_) => Status::InternalServerError
-	}
-}
+    #[get("/link/<id>")]
+    pub fn get_by_id(id: String) -> Status {
+        println!("{:?}", id);
+        //let link = get_link(id);
 
-fn get_tmp_file() -> Result<File> {
-	OpenOptions::new()
-		.read(true)
-		.write(true)
-		.create(true)
-		.open("/tmp/foo.txt")
+        Status::NoContent
+    }
+
+    #[get("/link")]
+    pub fn get_all() -> Result<Json<Vec<Link>>, rocket::http::Status> {
+        super::get_all_links()
+            .map(|links| Json(links))
+            .map_err(|_| Status::InternalServerError)
+    }
 }
 
 fn main() {
-	rocket::ignite().mount("/", routes![post]).launch();
+    rocket::ignite()
+        .mount(
+            "/",
+            routes![routes::post, routes::get_by_id, routes::get_all],
+        )
+        .launch();
+}
+
+fn save_link(link: &Link) -> Result<()> {
+    write!(get_tmp_file()?, "{},{}\n", link.url, link.read).map_err(|err| err.into())
+}
+
+fn get_all_links() -> Result<Vec<Link>> {
+    parse_file(&get_tmp_file()?)
+}
+
+fn parse_file(file: &File) -> Result<Vec<Link>> {
+    let mut link_vec: Vec<Link> = Vec::new();
+    let file = BufReader::new(file);
+
+    for line in file.lines() {
+        let line = line?;
+
+        let split = line.split(",").collect::<Vec<&str>>();
+
+        let read: bool = split[1].parse::<bool>()?;
+
+        let link = Link {
+            url: split[0].to_string(),
+            read,
+        };
+        link_vec.push(link);
+    }
+
+    Ok(link_vec)
+}
+
+fn get_tmp_file() -> Result<File> {
+    OpenOptions::new()
+        .read(true)
+        .create(true)
+        .append(true)
+        .open("/tmp/foo.txt")
+        .map_err(|err| err.into())
 }
