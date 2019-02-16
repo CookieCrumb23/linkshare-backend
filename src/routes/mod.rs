11fd8ea6d4468373153eba7155723715c1b4eb2d
CookieCrumb23 <CookieCrumb23@users.noreply.github.com>
2019-02-16 18:18:22 +0100

    Remove routes and io code from main package and move to routes package.

---
+++ src/routes/mod.rs
@@ -0,0 +1,68 @@
+extern crate md5;
+
+use serde_derive::{Deserialize, Serialize};
+use md5::{Md5, Digest};
+use rocket::http::Status;
+use std::result::Result;
+use std::collections::HashSet;
+
+use rocket_contrib::json::Json;
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Hash, Clone)]
+pub struct Link {
+    pub id: String,
+    pub url: String,
+    pub read: bool,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Url {
+    url: String
+}
+
+
+mod io;
+
+
+#[post("/link", data = "<url>")]
+pub fn post(url: Json<Url>) -> Result<Json<Link>, Status> {
+
+    let id = generate_id(&url.url);
+
+    let link = Link{url: url.url.to_owned(), read: false, id};
+
+    match io::save_link(&link) {
+        Ok(_) => Ok(Json(link)),
+        Err(_) => Err(Status::InternalServerError),
+    }
+}
+
+#[get("/link/<id>")]
+pub fn get_by_id(id: String) -> Result<Json<Link>, Status> {
+    io::get_link(&id)
+        .map(Json)
+        .ok_or(Status::NotFound)
+}
+
+#[get("/link")]
+pub fn get_all() -> Result<Json<HashSet<Link>>, Status> {
+    io::get_all_links()
+        .map(Json)
+        .map_err(|_| Status::InternalServerError)
+}
+
+#[put("/link/<id>/read")]
+pub fn set_read_state(id: String) -> Status {
+    println!("{:?}", id);
+    Status::NoContent
+}
+
+fn generate_id(url: &str) -> String {
+    format!(
+        "{:x}",
+        Md5::new()
+            .chain(url)
+            .result())
+        [0..8]
+        .to_owned()
+}
