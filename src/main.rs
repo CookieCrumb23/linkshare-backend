0bfc1c9d8bdfe740ee49a2a1e172e9290b97b8d0
CookieCrumb23 <CookieCrumb23@users.noreply.github.com>
2019-02-16 17:45:36 +0100

    Restructure handling of links and add id field.

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,84 @@
 #![feature(proc_macro_hygiene, decl_macro)]
-extern crate env_logger;
-extern crate log;
 #[macro_use]
 extern crate rocket;
+extern crate env_logger;
+extern crate log;
 
 use std::fs::{File, OpenOptions};
 use std::io::{BufRead, BufReader, Write};
 
 use serde_derive::{Deserialize, Serialize};
+use std::collections::HashSet;
 
 type Result<T> = std::result::Result<T, Box<std::error::Error>>;
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Hash, Clone)]
 pub struct Link {
+    id: String,
     url: String,
     read: bool,
 }
 
 mod routes {
+    extern crate md5;
+
+    use serde_derive::{Deserialize, Serialize};
+    use md5::{Md5, Digest};
     use rocket::http::Status;
     use std::result::Result;
+    use std::collections::HashSet;
 
     use rocket_contrib::json::Json;
+
     type Link = super::Link;
 
-    #[post("/link", data = "<link>")]
-    pub fn post(link: Json<Link>) -> Status {
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct Url {
+        url: String
+    }
+
+
+    #[post("/link", data = "<url>")]
+    pub fn post(url: Json<Url>) -> Result<Json<Link>, Status> {
+
+        let id = generate_id(&url.url);
+
+        let link = Link{url: url.url.to_owned(), read: false, id};
+
         match super::save_link(&link) {
-            Ok(_) => Status::NoContent,
-            Err(_) => Status::InternalServerError,
+            Ok(_) => Ok(Json(link)),
+            Err(_) => Err(Status::InternalServerError),
         }
     }
 
     #[get("/link/<id>")]
-    pub fn get_by_id(id: String) -> Status {
-        println!("{:?}", id);
-        //let link = get_link(id);
-
-        Status::NoContent
+    pub fn get_by_id(id: String) -> Result<Json<Link>, Status> {
+        super::get_link(&id)
+            .map(Json)
+            .ok_or(Status::NotFound)
     }
 
     #[get("/link")]
-    pub fn get_all() -> Result<Json<Vec<Link>>, rocket::http::Status> {
+    pub fn get_all() -> Result<Json<HashSet<Link>>, Status> {
         super::get_all_links()
-            .map(|links| Json(links))
+            .map(Json)
             .map_err(|_| Status::InternalServerError)
+    }
+
+    #[put("/link/<id>/read")]
+    pub fn set_read_state(id: String) -> Status {
+        println!("{:?}", id);
+        Status::NoContent
+    }
+
+    fn generate_id(url: &str) -> String {
+        format!(
+            "{:x}",
+            Md5::new()
+                .chain(url)
+                .result())
+            [0..8]
+            .to_owned()
     }
 }
 
@@ -52,38 +86,56 @@ fn main() {
     rocket::ignite()
         .mount(
             "/",
-            routes![routes::post, routes::get_by_id, routes::get_all],
+            routes![routes::post, routes::get_by_id, routes::get_all, routes::set_read_state],
         )
         .launch();
 }
 
 fn save_link(link: &Link) -> Result<()> {
-    write!(get_tmp_file()?, "{},{}\n", link.url, link.read).map_err(|err| err.into())
+    writeln!(get_tmp_file()?, "{},{},{}", link.id, link.url, link.read).map_err(Into::into)
 }
 
-fn get_all_links() -> Result<Vec<Link>> {
+fn get_all_links() -> Result<HashSet<Link>> {
     parse_file(&get_tmp_file()?)
 }
 
-fn parse_file(file: &File) -> Result<Vec<Link>> {
-    let mut link_vec: Vec<Link> = Vec::new();
+fn parse_file(file: &File) -> Result<HashSet<Link>> {
+    let mut link_set: HashSet<Link> = HashSet::new();
     let file = BufReader::new(file);
 
     for line in file.lines() {
         let line = line?;
 
-        let split = line.split(",").collect::<Vec<&str>>();
+        let split = line.split(',').collect::<Vec<&str>>();
 
-        let read: bool = split[1].parse::<bool>()?;
+        let read: bool = split[2].parse::<bool>()?;
 
         let link = Link {
-            url: split[0].to_string(),
+            id: split[0].to_string(),
+            url: split[1].to_string(),
             read,
         };
-        link_vec.push(link);
+
+        link_set.insert(link);
     }
 
-    Ok(link_vec)
+    Ok(link_set)
+}
+
+fn get_link(id: &str) -> Option<Link> {
+    let links = get_all_links();
+
+    if links.is_err() {
+        return None
+    }
+
+    for link in links.unwrap() {
+        if id == link.id {
+            return Some(link)
+        }
+    };
+
+    None
 }
 
 fn get_tmp_file() -> Result<File> {
@@ -92,5 +144,5 @@ fn get_tmp_file() -> Result<File> {
         .create(true)
         .append(true)
         .open("/tmp/foo.txt")
-        .map_err(|err| err.into())
+        .map_err(Into::into)
 }
